f555a1ba3387d1dce80f5f2c703ce0a8bb93c003
unknown <nmukhtar2001@gmail.com>
2023-01-01 15:23:13 -0800

    home work 3 done

---
+++ Develop/script.js
@@ -0,0 +1,87 @@
+var resultEl = document.getElementById('result');
+var lengthEl = document.getElementById('length');
+var uppercaseEl = document.getElementById('uppercase');
+var lowercaseEl = document.getElementById('lowercase');
+var numbersEl = document.getElementById('numbers');
+var symbolsEl = document.getElementById('symbols');
+var generateEl = document.getElementById('generate');
+var clipboard = document.getElementById('clipboard');
+
+var randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
+
+//Generate event listen
+clipboard.addEventListener('click', () => {
+	var textarea = document.createElement('textarea');
+	var password = resultEl.innerText;
+	
+	if(!password) { return; }
+	
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+});
+
+generate.addEventListener('click', () => {
+	var length = +lengthEl.value;
+	var hasLower = lowercaseEl.checked;
+	var hasUpper = uppercaseEl.checked;
+	var hasNumber = numbersEl.checked;
+	var hasSymbol = symbolsEl.checked;
+	
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+// Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+    // 1. Init pw var
+    // 2. Filter out unchecked types
+    // 3. Loop over length call generator function for each type
+    // 4. Add final pw to the pw var and return
+	let generatedPassword = '';
+	var typesCount = lower + upper + number + symbol;
+	var typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	// Doesn't have a selected type
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			var funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	var finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
+
+// Generator functions
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	var symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
