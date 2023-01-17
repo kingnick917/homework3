995979463f3939ad388d4fd1dcba6eadfaf3f9b9
unknown <nmukhtar2001@gmail.com>
2023-01-16 16:55:33 -0800

    add checkbox to symbol to html

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#wrapper");
-var generateEl  = document.querySelector("#card");
-var cardEL = document.querySelector("#body");
-var footerEL =document.querySelector("#button");
-var lengthEl = document.getElementById('#length');
-var uppercaseEl = document.getElementById('#uppercase');
-var lowercaseEl = document.getElementById('#lowercase');
-var numbersEl = document.getElementById('#numbers');
-var symbolsEl = document.getElementById('#symbols');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -17,7 +9,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
