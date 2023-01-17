d9bb0533c56190848b9a3752bc86d50a6dde7334
unknown <nmukhtar2001@gmail.com>
2023-01-16 16:25:36 -0800

    add optoins for the password

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#wrapper");
-var generateEl  = document.querySelector("#card") 
-var cardEL = document.querySelector("body")
-var card-footer =document.querySelector("button")
+var generateEl  = document.querySelector("#card");
+var cardEL = document.querySelector("#body");
+var footerEL =document.querySelector("#button");
+var lengthEl = document.getElementById('#length');
+var uppercaseEl = document.getElementById('#uppercase');
+var lowercaseEl = document.getElementById('#lowercase');
+var numbersEl = document.getElementById('#numbers');
+var symbolsEl = document.getElementById('#symbols');
 
 // Write password to the #password input
 function writePassword() {
@@ -12,6 +17,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
