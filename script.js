df0cfa29d3d01096d220177b1c97cef8ae23b15e
unknown <nmukhtar2001@gmail.com>
2023-01-16 16:17:58 -0800

    add some new code to js

--- script.js
+++ script.js
@@ -1,84 +1,17 @@
-var resultEl = document.getElementById('result');
-var lengthEl = document.getElementById('length');
-var uppercaseEl = document.getElementById('uppercase');
-var lowercaseEl = document.getElementById('lowercase');
-var numbersEl = document.getElementById('numbers');
-var symbolsEl = document.getElementById('symbols');
-var generateEl = document.getElementById('generate');
-var clipboard = document.getElementById('clipboard');
+// Assignment Code
+var generateBtn = document.querySelector("#wrapper");
+var generateEl  = document.querySelector("#card") 
+var cardEL = document.querySelector("body")
+var card-footer =document.querySelector("button")
 
-var randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
-
-clipboard.addEventListener('click', () => {
-	var textarea = document.createElement('textarea');
-	var password = resultEl.innerText;
-	
-	if(!password) { return; }
-	
-	textarea.value = password;
-	document.body.appendChild(textarea);
-	textarea.select();
-	document.execCommand('copy');
-	textarea.remove();
-	alert('Password copied to clipboard');
-});
-
-generate.addEventListener('click', () => {
-	var length = +lengthEl.value;
-	var hasLower = lowercaseEl.checked;
-	var hasUpper = uppercaseEl.checked;
-	var hasNumber = numbersEl.checked;
-	var hasSymbol = symbolsEl.checked;
-	
-	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-});
-
-
-function generatePassword(lower, upper, number, symbol, length) {
-  
-	let generatedPassword = '';
-	var typesCount = lower + upper + number + symbol;
-	var typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-	
-
-	if(typesCount === 0) {
-		return '';
-	}
-	
-	// create a loop
-	for(let i=0; i<length; i+=typesCount) {
-		typesArr.forEach(type => {
-			var funcName = Object.keys(type)[0];
-			generatedPassword += randomFunc[funcName]();
-		});
-	}
-	
-	var finalPassword = generatedPassword.slice(0, length);
-	
-	return finalPassword;
-}
-
-
-
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-	var symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
