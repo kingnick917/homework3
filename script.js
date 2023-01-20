957b5b772f4ac98fc3217800e3c28f3f1cda176d
unknown <nmukhtar2001@gmail.com>
2023-01-19 22:28:10 -0800

    added new commit in js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ var lowercaseEl = document.querySelector('#lowercase');
 var numbersEl = document.querySelector('#numbers');
 var symbolsEl = document.querySelector('#symbols');
 var resultEl = document.querySelector('#result');
-var clipboard = document.querySelector('#clipboard');
 
 
+// this function is runs the password text 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -25,24 +25,10 @@ var randomFunc = {
 }
 
 
-clipboard.addEventListener('click', () => {
-    var textarea = document.createElement('textarea');
-    var password = resultEl.innerText;
-   
-    if(!password) { return; }
-   
-    textarea.value = password;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    textarea.remove();
-    alert('Password copied to clipboard');
-});
 
 
 
-
-generate.addEventListener('click', () => {
+generate.addEventListener('click', function () {
     var length = +lengthEl.value;
     var hasLower = lowercaseEl.checked;
     var hasUpper = uppercaseEl.checked;
@@ -53,45 +39,59 @@ generate.addEventListener('click', () => {
 });
 // these are the buttons to selcet options
 
-
+// this is generating password based off of checkboxes on my form.
 function generatePassword(lower, upper, number, symbol, length) {
- 
-    let generatedPassword = '';
+    
+ //
+    let newPass = '';
     var typesCount = lower + upper + number + symbol;
-    var typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+  
+    var typesArr = [];
+
+    if (lower){
+        typesArr.push({lower});
+    }
    
- 
+    if (upper){
+        typesArr.push({upper});
+    }
+    if (number){
+        typesArr.push({number});
+    }
+    if (symbol){
+        typesArr.push({symbol});
+    }
     if(typesCount === 0) {
         return '';
     }
    
 
     for(let i=0; i<length; i+=typesCount) {
-        typesArr.forEach(type => {
+        typesArr.forEach(function (type) {
             var funcName = Object.keys(type)[0];
-            generatedPassword += randomFunc[funcName]();
+            newPass += randomFunc[funcName]();
         });
     }
    
-    var finalPassword = generatedPassword.slice(0, length);
+    var finalPassword = newPass.slice(0, length);
    
     return finalPassword;
 }
  
  
- 
+ // uses ascII to get lowercase numbers lower case numbers start at 98;
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
- 
+ //26 is the sub number and 65 is upper A 
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
- 
+ // the number 10 is LF 48 is  0 
 function getRandomNumber() {
     return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
- 
+ //  this is for the Symbol when then the button is clicked this will run
 function getRandomSymbol() {
     var symbols = '!@#$%^&*(){}[]=<>/,.'
     return symbols[Math.floor(Math.random() * symbols.length)];
